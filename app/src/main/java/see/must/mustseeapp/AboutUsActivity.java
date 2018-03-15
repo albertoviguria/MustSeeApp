7b0d4d0dba74d45ce8a7791b3c148f281ce7a552
iLerga97 <36731319+iLerga97@users.noreply.github.com>
2018-03-15 17:16:01 +0100

    Inigo (#1)
    * Recuperando versión original con AboutUs
    
    * Recuperando versión original

---
+++ app/src/main/java/see/must/mustseeapp/AboutUsActivity.java
@@ -0,0 +1,15 @@
+package see.must.mustseeapp;
+import android.app.Activity;
+import android.os.Bundle;
+/**
+ * Created by ilerg on 15/03/2018.
+ */
+
+public class AboutUsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.aboutus);
+    }
+}
--- app/src/main/java/see/must/mustseeapp/MainActivity.java
+++ app/src/main/java/see/must/mustseeapp/MainActivity.java
@@ -1,5 +1,6 @@
 package see.must.mustseeapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -15,6 +16,8 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private static final int SHOW_ABOUTUSACTIVITY = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -80,18 +83,14 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.buscar) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.historial) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.aboutUs) {
 
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+            Intent intent = new Intent(this,AboutUsActivity.class);
+            startActivityForResult(intent, SHOW_ABOUTUSACTIVITY);
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
